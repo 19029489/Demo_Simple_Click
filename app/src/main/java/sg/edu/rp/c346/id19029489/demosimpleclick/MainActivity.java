ba2f0d3c8548fc622fca73e371e96807679e3c1f
19029489 <19029489@rp.edu.sg>
2020-05-08 21:40:12 +0800

    Word entered gets displayed when button is pressed/clicked. Toggle Button that enables and disables the text input box.

--- app/src/main/java/sg/edu/rp/c346/id19029489/demosimpleclick/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id19029489/demosimpleclick/MainActivity.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
     TextView tvDisplay;
     Button btnDisplay;
     EditText etInput;
+    ToggleButton tbtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
         tvDisplay = findViewById(R.id.textViewDisplay);
         btnDisplay = findViewById(R.id.buttonDisplay);
         etInput = findViewById(R.id.editTextInput);
+        tbtn = findViewById(R.id.toggleButtonEnabled);
 
         btnDisplay.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -32,7 +35,20 @@ public class MainActivity extends AppCompatActivity {
                 // Code for the action
                 String stringResponse = etInput.getText().toString();
                 tvDisplay.setText(stringResponse);
-                
+            }
+        });
+
+        tbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (tbtn.isChecked() == true){
+                    etInput.setEnabled(true);
+                }
+                else{
+                    etInput.setEnabled(false);
+                }
+                //can also etInput.setEnabled(tbtn.isChecked());
+
             }
         });
     }
